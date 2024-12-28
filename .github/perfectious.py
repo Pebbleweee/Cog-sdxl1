6dba141fa2991faa4eb8fe0ad6c3d0346f0232a4
Pebbleweee <opuspleaseaaaaaaaa@proton.me>
2024-12-28 20:58:22 +0300

    Update perfectious.py

--- .github/perfectious.py
+++ .github/perfectious.py
@@ -9,8 +9,7 @@ def main(args):
   _util.replace_defaults({
     'DEFAULT_VAE_NAME': None,
     'DEFAULT_POS_PREPROMPT': "masterpiece, best quality, amazing quality, very aesthetic, absurdres,",
-    'DEFAULT_NEG_PREPROMPT': "<low quality, worst quality, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, jpeg artifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, extra limbs, extra arms, extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed, mutated hands, polar lowres, bad body, bad proportions, gross proportions, wrong shadows>
-",
+    'DEFAULT_NEG_PREPROMPT': "low quality, worst quality, bad anatomy, bad hands,
     'DEFAULT_POSITIVE_PROMPT': "",
     'DEFAULT_NEGATIVE_PROMPT': "",
     'DEFAULT_CFG': 6,
